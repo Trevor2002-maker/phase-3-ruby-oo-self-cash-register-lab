e8cedacae6595f608877914e5cca96ed5be0bb0e
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-22 11:09:02 +0300

    Done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,50 +1,66 @@
 class CashRegister
-    def initialize
+    attr_reader :total, :items
+  
+    def initialize(employee_discount = 0)
+      @total = 0
       @items = []
-      @transactions = []
+      @employee_discount = employee_discount
     end
   
-    def add_item(name, quantity, price)
-      item = { name: name, quantity: quantity, price: price }
+    def add_item(title, price, quantity = 1)
+      item = { title: title, price: price, quantity: quantity }
       @items << item
+      @total += price * quantity
     end
   
-    def calculate_discount(discount_percentage)
-      total = calculate_total
-      discount_amount = total * (discount_percentage / 100.0)
-      total - discount_amount
+    def apply_discount
+      if @employee_discount > 0
+        discount_amount = @total * @employee_discount / 100.0
+        @total -= discount_amount
+        "After applying a #{@employee_discount}% discount, the total is $#{@total}."
+      else
+        "Sorry, there is no discount to apply."
+      end
     end
   
     def void_last_transaction
-      last_transaction = @transactions.pop
-      last_transaction[:quantity].times { @items.pop }
-    end
-  
-    def print_receipt
-      puts "==== Receipt ===="
-      @items.each do |item|
-        puts "#{item[:name]} (x#{item[:quantity]}) - $#{item[:price] * item[:quantity]}"
-      end
-      puts "Total: $#{calculate_total}"
-      puts "==============="
-    end
-  
-    private
-  
-    def calculate_total
-      @items.reduce(0) { |total, item| total + (item[:price] * item[:quantity]) }
+      last_item = @items.pop
+      @total -= last_item[:price] * last_item[:quantity]
+      @total = 0.0 if @items.empty?
     end
   end
-  
-  # Example usage:
-  
-  register = CashRegister.new
-  register.add_item("Apple", 3, 1.50)
-  register.add_item("Banana", 2, 0.75)
-  register.print_receipt
-  
-  discounted_total = register.calculate_discount(10) # Apply 10% discount
-  puts "Discounted Total: $#{discounted_total}"
-  
-  register.void_last_transaction
-  register.print_receipt
+# Creating a CashRegister instance with a 20% employee discount
+register = CashRegister.new(20)
+
+# Adding items to the cash register
+register.add_item("Apple", 3)
+register.add_item("Banana", 2, 2)
+register.add_item("Orange", 4, 3)
+
+# Printing the current total
+puts "Current Total: $#{register.total}"
+# Output: Current Total: $21
+
+# Applying the discount
+puts register.apply_discount
+# Output: After applying a 20% discount, the total is $16.8
+
+# Printing the list of items
+puts "Items: #{register.items}"
+# Output: Items: [{:title=>"Apple", :price=>3, :quantity=>1}, {:title=>"Banana", :price=>2, :quantity=>2}, {:title=>"Orange", :price=>4, :quantity=>3}]
+
+# Voiding the last transaction
+register.void_last_transaction
+
+# Printing the updated total after voiding the last transaction
+puts "Updated Total: $#{register.total}"
+# Output: Updated Total: $12
+
+# Voiding all transactions
+register.void_last_transaction
+register.void_last_transaction
+register.void_last_transaction
+
+# Printing the total after voiding all transactions
+puts "Total after voiding all transactions: $#{register.total}"
+# Output: Total after voiding all transactions: $0.0
